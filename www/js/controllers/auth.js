11ba072649f96ae20c83736cd86c72a14ab37cac
Keven Lee <kevenlee123@gmail.com>
2016-06-02 18:37:39 -0400

    first commit

---
+++ www/js/controllers/auth.js
@@ -0,0 +1,23 @@
+'use strict';
+
+app.controller('AuthCtrl', function(Auth, $state) {
+               
+    var auth = this;
+               
+    auth.login = function() {
+    console.log('Login clicked');
+    
+    return Auth.login().then(function(user) {
+        $state.go('app.home');
+    });
+        
+    };
+
+    auth.logout = function(){
+    Auth.logout();
+    };
+});
+               
+               
+               
+               
